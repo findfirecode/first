fba0922178d07526759fa59fa7b5ae4492af5778
邓学长 <976104701@qq.com>
2019-09-22 13:18:15 +0800

    add cli3 config

---
+++ server.js
@@ -0,0 +1,29 @@
+const Vue = require("vue");
+
+const Koa = require("koa");
+const app = new Koa();
+const renderer = require("vue-server-renderer").createRenderer();
+
+// 第 3 步：添加一个中间件来处理所有请求
+app.use(async (ctx, next) => {
+  const vm = new Vue({
+    data: {
+      title: "ssr example",
+      url: ctx.url
+    },
+    template: `<div>访问的 URL 是： {{ url }}</div>`
+  });
+  // 将 Vue 实例渲染为 HTML
+  renderer.renderToString(vm, (err, html) => {
+    if(err){
+      ctx.res.status(500).end('Internal Server Error')
+      return
+    }
+    ctx.body = html
+  });
+});
+
+const port = 3000;
+app.listen(port, function() {
+  console.log(`server started at localhost:${port}`);
+});
--- src/entry-server.js
+++ src/entry-server.js
@@ -1,4 +1,4 @@
-import createApp from './App';
+import createApp from './main';
 
 export default context => new Promise((resolve, reject) => {
   const { app, router, store } = createApp();
@@ -7,7 +7,17 @@ export default context => new Promise((resolve, reject) => {
     const matchComponent = router.getMatchedComponents();
 
     if (!matchComponent.length) {
-      // reject({ code: '404' });
+      reject({ code: '404' });
     }
+
+    Promise.all(matchComponent.map((component) =>
+      component.asyncData({
+        store,
+        route:router.currentRoute
+      })
+    )).then((res) => {
+      context.state = store.state
+      resolve(app)
+    })
   });
 });
